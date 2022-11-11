758fb1908ca47f22cf1d1e1e54c0f76ec8261cd2
oliveiramatheux <matheus.oliveira0730@gmail.com>
2022-11-11 19:01:15 -0300

    att presentation

--- bookUser/src/main/java/com/microservices/controller/Controller.java
+++ bookUser/src/main/java/com/microservices/controller/Controller.java
@@ -3,6 +3,7 @@ package com.microservices.controller;
 import com.microservices.entity.BookUser;
 import com.microservices.repository.BookUserRepository;
 import com.microservices.vo.UserVO;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +16,20 @@ public class Controller {
 
     @RequestMapping(value = "/users", method = RequestMethod.POST)
     public ResponseEntity<?> save(@RequestBody UserVO user) {
+        if(user.getEmail() == null) return ResponseEntity.badRequest().body("You must provide an email");
+
+        Optional<BookUser> userExist = bookUserRepository.findByEmail(user.getEmail()).stream().findFirst();
+
+        if(userExist.equals(Optional.empty()) == false) return ResponseEntity.badRequest().body("User already exist!");
+
         BookUser bookUser = new BookUser();
         return ResponseEntity.ok(bookUserRepository.save(bookUser.convert(user)));
     }
 
-    @RequestMapping(value = "/users/update", method = RequestMethod.POST)
+    @RequestMapping(value = "/users", method = RequestMethod.PATCH)
     public ResponseEntity<?> update(@RequestBody UserVO user) {
-        if(user.getId() == null){
-            return ResponseEntity.badRequest().body("You must provide an ID");
-        }
+        if(user.getId() == null) return ResponseEntity.badRequest().body("You must provide an ID");
+
         BookUser bookUser = new BookUser();
         return ResponseEntity.ok(bookUserRepository.save(bookUser.convert(user)));
     }
@@ -39,7 +45,7 @@ public class Controller {
         if(id == null) return ResponseEntity.badRequest().body("ID not valid");
 
         Integer userId = Integer.parseInt(id);
-        return ResponseEntity.ok(bookUserRepository.findById(userId.longValue())); 
+        return ResponseEntity.ok(bookUserRepository.findById(userId.longValue()));
     }
 
     @RequestMapping(value = "/users/email/{email}", method = RequestMethod.GET)
@@ -54,6 +60,6 @@ public class Controller {
 
         Integer userId = Integer.parseInt(id);
         bookUserRepository.deleteById(userId.longValue());
-        return ResponseEntity.ok("User deleted"); 
+        return ResponseEntity.ok("User deleted");
     }
 }
